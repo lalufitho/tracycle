d05294da5533ad364da0aa9664667e4bba1d81b1
Dena Indiarto Permadi <denaindiartopermadi2002@gmail.com>
2023-05-30 23:17:03 +0800

    homepage

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -1,22 +1,36 @@
 const DrawerInitiator = {
-  init({ button, drawer, content }) {
-    button.addEventListener('click', (event) => {
-      this._toggleDrawer(event, drawer);
-    });
+  init({ menuButton, closeMenu, drawer }) {
+    const navBeranda = document.getElementsByClassName('nav__item')[0];
+    const navBankSampah = document.getElementsByClassName('nav__item')[1];
+    const navDaurUlang = document.getElementsByClassName('nav__item')[2];
+    const navTentang = document.getElementsByClassName('nav__item')[3];
+    const navKontak = document.getElementsByClassName('nav__item')[4];
 
-    content.addEventListener('click', (event) => {
-      this._closeDrawer(event, drawer);
-    });
+    function drawerToggle(element, method) {
+      element.addEventListener('click', (event) => {
+        method(event, drawer);
+      });
+    }
+
+    drawerToggle(menuButton, this._toggleDrawer);
+    drawerToggle(closeMenu, this._closeDrawer);
+    drawerToggle(navBeranda, this._closeDrawer);
+    drawerToggle(navBankSampah, this._closeDrawer);
+    drawerToggle(navDaurUlang, this._closeDrawer);
+    drawerToggle(navTentang, this._closeDrawer);
+    drawerToggle(navKontak, this._closeDrawer);
   },
 
   _toggleDrawer(event, drawer) {
     event.stopPropagation();
     drawer.classList.toggle('open');
+    document.body.style.overflow = 'hidden';
   },
 
   _closeDrawer(event, drawer) {
     event.stopPropagation();
     drawer.classList.remove('open');
+    document.body.style.overflow = 'scroll';
   },
 };
 
